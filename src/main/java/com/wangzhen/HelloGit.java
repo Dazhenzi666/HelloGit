1d132013f9a6c444fc4a5d028613c663d005a2de
撒娇 <Dazhenzi666@163.com>
2020-06-14 13:42:12 +0800

    更新1

--- src/main/java/com/wangzhen/HelloGit.java
+++ src/main/java/com/wangzhen/HelloGit.java
@@ -3,5 +3,7 @@ package com.wangzhen;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git hub");
+        System.out.println("更新一");
+
     }
 }
